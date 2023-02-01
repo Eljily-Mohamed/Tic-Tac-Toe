75bebe9d7ffce1331a48b5f2ede794873284dc81
hamade-kj <hamadekj15@gmail.com>
2023-02-01 13:33:19 +0100

    get update

--- src/Composants/maingameui.js
+++ src/Composants/maingameui.js
@@ -4,7 +4,7 @@ const Maingameui = function ({game}) {
     //on can declare function pour recupere la valeur de notre variables 
     const [mark , setMark ] = useState (game.mark) ;
     const [player , setPlayer ] = useState(game.player);
-
+    const places = ['0', '1', '2', '3','4','5' ,'6','7'];
     return (
         <div  className="maingameui">
              <div className="view-game">
@@ -17,10 +17,10 @@ const Maingameui = function ({game}) {
                        <p>O's Turn</p>
                    </div>
                 </div>
-
                 {/* Border jeux */}
                 <div className="Border" >
                     {/* differents place for play */}
+                    {places.map((elem) => )})
                 </div>
              </div>
         </div>
